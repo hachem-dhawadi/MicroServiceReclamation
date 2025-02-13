6115aa4379e1f15ed9cb8407e269d67082a81cf7
hachem-dhawadi <hachemdhawadi2001@gmail.com>
2025-02-13 11:37:09 +0100

    eureka config

--- src/main/java/com/esprit/microservice/recllamation/RecllamationApplication.java
+++ src/main/java/com/esprit/microservice/recllamation/RecllamationApplication.java
@@ -2,12 +2,13 @@ package com.esprit.microservice.recllamation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RecllamationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RecllamationApplication.class, args);
     }
-
 }
